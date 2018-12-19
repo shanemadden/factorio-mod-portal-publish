2417e64a3b30e495d82964c4c1fa7958e7534838
Shane Madden <shanemadden@fake.email>
2018-12-18 21:07:37 -0700

    More prints

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 sh -c "echo $*"
 sh -c "echo \"${GITHUB_REF}\""
@@ -12,3 +13,5 @@ sh -c "zip -r $1_${TAG}.zip . $1 -x \*.git\*"
 sh -c "ls -la"
 
 sh -c "curl -v -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN}"
+
+sh -c "echo done"
