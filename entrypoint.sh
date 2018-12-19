2e600562a6458c8b129d49a8d2c2377d96d64623
Shane Madden <shanemadden@fake.email>
2018-12-18 23:00:14 -0700

    See what this does without all the sh -c wrapping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-sh -c "echo $*"
-sh -c "echo \"${GITHUB_REF}\""
-
 TAG=$(sh -c "echo \"${GITHUB_REF}\" | grep tags | grep -o \"[^/]*$\"")
 
-sh -c "echo \"${TAG}\""
+zip -r $1_${TAG}.zip . $1 -x \*.git\*
 
-sh -c "zip -r $1_${TAG}.zip . $1 -x \*.git\*"
-
-sh -c "ls -la"
-
-sh -c "curl -v google.com"
+curl -v google.com
 
 #sh -c "curl -v -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN}"
-sh -c "curl -v https://mods.factorio.com/mod/$1/downloads/edit"
-
-sh -c "echo done"
+curl -v https://mods.factorio.com/mod/$1/downloads/edit
