fc325a409950f9daf5e5da259456705fe973a519
Shane Madden <shanemadden@fake.email>
2018-12-18 20:39:48 -0700

    Fix string end matching

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 sh -c "echo $*"
 sh -c "echo \"${GITHUB_REF}\""
-sh -c "TAG=$(echo \"${GITHUB_REF}\" | grep tags | grep -o \"[^/]*\" ) && echo \"building ${TAG}\" && zip -r $1_${TAG}.zip . $1 -x \*.git\* && curl -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN} && echo done"
+sh -c "TAG=$(echo \"${GITHUB_REF}\" | grep tags | grep -o \"[^/]*$\" ) && echo \"building ${TAG}\" && zip -r $1_${TAG}.zip . $1 -x \*.git\* && curl -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN} && echo done"
