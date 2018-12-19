6cb8ffbd288e13f47f1b4cc89fd128cb3f686043
Shane Madden <shanemadden@fake.email>
2018-12-18 20:14:15 -0700

    More debug print

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "TAG=$(echo \"${GITHUB_REF}\" | grep tags | grep -o \"[^/]*\" ) && echo \"building ${TAG}\" && zip -r $1_${TAG}.zip . $1 -x \*.git\* && curl -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN} && echo done"
+sh -c "echo \"${GITHUB_REF}\" && TAG=$(echo \"${GITHUB_REF}\" | grep tags | grep -o \"[^/]*\" ) && echo \"building ${TAG}\" && zip -r $1_${TAG}.zip . $1 -x \*.git\* && curl -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN} && echo done"
