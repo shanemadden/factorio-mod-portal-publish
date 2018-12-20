a00f33cfd264f7540436328d0f1f5001c8408b52
Shane Madden <shanemadden@fake.email>
2018-12-19 17:13:47 -0700

    Attempt to get working login/session/upload logic, plus some error checking

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM debian:9-slim
 
 LABEL "com.github.actions.name"="Factorio Mod Portal Publish"
-LABEL "com.github.actions.description"="Publishes zipped archives to the Factorio mod portal"
+LABEL "com.github.actions.description"="Publishes repos with Factorio mods to the Factorio mod portal"
 LABEL "com.github.actions.icon"="settings"
 LABEL "com.github.actions.color"="orange"
 
 LABEL "repository"="https://github.com/shanemadden/factorio-mod-portal-publish"
-LABEL "maintainer"="Shane Madden <shanemadden@fake.email>"
+LABEL "maintainer"="Shane Madden"
 
-RUN apt-get update && apt-get -y install curl zip
+RUN apt-get update && apt-get -y install curl zip jq
 
 ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,63 @@
 #!/bin/sh
 
-TAG=$(sh -c "echo \"${GITHUB_REF}\" | grep tags | grep -o \"[^/]*$\"")
+# Parse the tag we're on, do nothing if there isn't one
+TAG=$(echo ${GITHUB_REF} | grep tags | grep -o "[^/]*$")
+if [ -z "${TAG}" ]; then
+    echo "Not a tag push, skipping"
+    exit 78
+fi
 
-zip -r $1_${TAG}.zip . $1 -x \*.git\*
+# Validate the version string we're building
+if ! echo "${TAG}" | grep -P --quiet '^\d+\.\d+\.\d+$'; then
+    echo "Bad version, needs to be %u.%u.%u."
+    exit 1
+fi
 
-curl -v google.com
+# Make sure the info.json is parseable and has the expected version number
+if [ $(jq '.version' info.json) -ne "${TAG}" ]; then
+    echo "Tag version doesn't match info.json (or info.json is invalid), failed"
+    exit 1
+fi
+# Create the zip
+zip -r "$1_${TAG}.zip" . $1 -x \*.git\*
 
-#sh -c "curl -v -X POST -F \"file=@$1_${TAG}.zip\" https://mods.factorio.com/mod/$1/downloads/edit?username=${FACTORIO_USER}&token=${FACTORIO_TOKEN}"
-curl -v https://mods.factorio.com/mod/$1/downloads/edit
+# Get a CSRF token by loading the login form
+CSRF=$(curl -b cookiejar.txt -c cookiejar.txt -s https://mods.factorio.com/login | grep csrf_token | sed -r -e 's/.*value="(.*)".*/\1/')
+
+# Authenticate with the credential secrets and the CSRF token, getting a session cookie for the authorized user
+curl -b cookiejar.txt -c cookiejar.txt -s -F "csrf_token=${CSRF}" -F "username=${FACTORIO_USER}" -F "password=${FACTORIO_PASSWORD}" -o /dev/null https://mods.factorio.com/login
+
+# Query the mod info, verify the version number we're trying to push doesn't already exist
+curl -b cookiejar.txt -c cookiejar.txt -s https://mods.factorio.com/api/mods/$1/full | jq -e ".releases[] | select(.version == \"${TAG}\")"
+# store the return code before running anything else
+STATUS_CODE=$?
+
+if [ $STATUS_CODE -ne 4 ]; then
+    echo "Release already exists, skipping"
+    exit 78
+fi
+
+# Load the upload form, getting an upload token
+UPLOAD_TOKEN=$(curl -b cookiejar.txt -c cookiejar.txt -s https://mods.factorio.com/mod/$1/downloads/edit | grep token | sed -r -e "s/.*token: '(.*)'.*/\1/")
+if [ -z "${UPLOAD_TOKEN}" ]; then
+    echo "Couldn't get an upload token, failed"
+    exit 1
+fi
+
+# Upload the file, getting back a response with details to send in the final form submission to complete the upload
+UPLOAD_RESULT=$(curl -b cookiejar.txt -c cookiejar.txt -s -F "file=@$1_${TAG}.zip;type=application/x-zip-compressed" "https://direct.mods-data.factorio.com/upload/mod/${UPLOAD_TOKEN}")
+
+# Parse 'em and stat the file for the form fields
+CHANGELOG=$(echo ${UPLOAD_RESULT} | jq '.changelog')
+INFO=$(echo ${UPLOAD_RESULT} | jq '.info')
+FILENAME=$(echo ${UPLOAD_RESULT} | jq '.filename')
+FILESIZE=$(stat --printf="%s" "$1_${TAG}.zip")
+
+if [ -z "${FILENAME}" ]; then
+    echo "Upload failed"
+    exit 1
+fi
+# Post the form, completing the release
+curl -b cookiejar.txt -c cookiejar.txt -s -F "file=" -F "info_json=${INFO}" -F "changelog=${CHANGELOG}" -F "filename=${FILENAME}" -F "file_size=${FILESIZE}" -o /dev/null https://mods.factorio.com/mod/$1/downloads/edit
+
+echo "Completed"
